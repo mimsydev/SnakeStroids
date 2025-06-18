0732a61bf41c9730f8b2eb23efafbaa1d7a89b71
Johnathan Rhoades <jrhoades7989@gmail.com>
2025-06-17 21:23:12 -0400

    added framerate

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ from typing import Tuple
 def main() -> None:
     (passnum, failnum) = pygame.init()
     screen: pygame.Surface = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock: pygame.time.Clock = pygame.time.Clock()
+    dt: int = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
